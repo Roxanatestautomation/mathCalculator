4ed04f738bd951192826c1899c6d91202a25d5e5
Roxana <roxanacroitoru378@yahoo.ro>
2021-01-08 11:04:19 +0000

    added and invoke methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,28 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Components components = new Components();
+        int a = 4;
+        int b = 2;
+        int sum = a+b;
+        int sub = a-b;
+        int mut = a*b;
+
+        components.addition(a,b);
+        System.out.println(sum);
+
+        components.subtraction(a,b);
+        System.out.println(sub);
+
+        components.multiplication(a,b);
+        System.out.println(mut);
+
+
+
+
+
     }
 }
---
+++ src/main/java/org/fasttrackit/Components.java
@@ -0,0 +1,19 @@
+package org.fasttrackit;
+
+public class Components {
+
+    int a;
+    int b;
+
+    public int addition (int a, int b) {
+        return 0;
+    }
+
+    public int subtraction (int a, int b) {
+        return 0;
+    }
+
+    public int multiplication (int a, int b){
+        return 0;
+    }
+}
